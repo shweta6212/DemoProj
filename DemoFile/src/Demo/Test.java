9c09161cc6d93ccbd7f6fe8def032367157e341f
shweta6212 <104562669+shweta6212@users.noreply.github.com>
2023-02-03 16:56:36 +0530

    Add files via upload

---
+++ DemoFile/src/Demo/Test.java
@@ -0,0 +1,21 @@
+package Demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Shweta";
+		System.out.println(name);
+		
+		if(name.equals("Shweta")) {
+			System.out.println("true");
+		}
+			else {
+				
+			System.out.println("false");
+			}
+				
+		}
+	}
+
+
